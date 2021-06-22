7ae48ead4b35c81500016ba86f20e7f9a5e46ed8
kunkun9811 <timochen77@gmail.com>
2021-06-21 18:10:37 -0700

    New file structure

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import App from "./App";
+import App from "../components/App";
 
 test("Check that the main title is correct", () => {
   const { getByTestId } = render(<App />);
