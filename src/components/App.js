d960d435ad38193334779f5ba3e0431c817f79a2
kunkun9811 <timochen77@gmail.com>
2021-06-22 17:04:43 -0700

    Added Map

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
+import React, { useState, useEffect } from "react";
+import HeatMap from "./HeatMap.jsx";
+
 function App() {
+  // states
+  const [covidData, setCovidData] = useState([]);
+
+  // Fetch COVID data
+  // useEffect(async () => {
+  //   await fetch("https://www.covid19tracker.kuundev.com/api/all/")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log("***** Response from API *****");
+  //       console.log(data);
+  //       console.log("***** END for Response from API *****");
+
+  //       setCovidData(data);
+  //     });
+  // });
+
   return (
     <div>
-      <h1 data-testid="title">COVID19 Tracker</h1>
+      <HeatMap></HeatMap>
     </div>
   );
 }
---
+++ src/components/HeatMap.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+
+function HeatMap() {
+  const [viewport, setViewport] = useState({
+    latitude: 23.6978,
+    longitude: 120.9605,
+    width: "100vw",
+    height: "100vh",
+    zoom: 6,
+  });
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/springkun/ckq8p5a2512vd19q0w156ebkt"
+        onViewportChange={(NewViewport) => {
+          setViewport(NewViewport);
+        }}
+      ></ReactMapGL>
+    </div>
+  );
+}
+
+export default HeatMap;
